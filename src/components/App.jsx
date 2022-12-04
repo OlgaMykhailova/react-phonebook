6c87898bbb0bf388e136787274e49aa2b84b9ca6
OlgaMykhailova <rosebud_z@yahoo.com>
2022-12-04 13:59:31 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,7 +25,7 @@ export const App = () => {
     <Container
       maxW="container.lg"
       minH="container.lg"
-      maxW={['400px', null, '800px', null]}
+      minW={['400px', null, '800px', null]}
       p="7"
       boxShadow="dark-lg"
       bg="teal.300"
